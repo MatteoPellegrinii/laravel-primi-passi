27db6d9a9f54b159379607cd8db9381003ac9d97
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-07-11 15:21:28 +0200

    fine esercizio

---
+++ resources/views/helloworld.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="links">
+        <a href="/links">Links</a>
+        <a href="/about">About us</a>
+    </header>
+    <h1>HELLO WORLD</h1>
+    
+</body>
+</html>
+
+<style>
+.links > a {
+color: #636b6f;
+padding: 0 25px;
+font-size: 13px;
+font-weight: 600;
+letter-spacing: .1rem;
+text-decoration: none;
+text-transform: uppercase;
+}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('helloworld');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/links', function () {
+    return view('links');
 });
